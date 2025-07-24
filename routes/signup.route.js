731cb6c8d12960c9dd3f78f931072c6677f56694
Akshay Reddy <poddukuriakshayreddy1605@gmail.com>
2025-07-24 17:54:35 +0530

    Update signup.route.js

--- routes/signup.route.js
+++ routes/signup.route.js
@@ -1,7 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const signupController = require('../controllers/signup.controller');
 
-router.post('/', signupController.signup);
-
-module.exports = router;
