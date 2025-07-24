aab5cf716925092ec9f29eeb925fab7846a3fac3
Akshay Reddy <poddukuriakshayreddy1605@gmail.com>
2025-07-24 17:53:44 +0530

    Update login.controller.js

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -1,12 +1,1 @@
-// const User = require('../models/login.model.js');
-const User = require('../models/user.model.js');
-const bcrypt = require('bcrypt');
 
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) return res.status(401).json({ error: 'Invalid credentials.' });
-  const match = await bcrypt.compare(password, user.password);
-  if (!match) return res.status(401).json({ error: 'Invalid credentials.' });
-  res.json({ message: 'Login successful.' });
-};
